9d10689b71fd57b5fdb64aa39fbd40a1164223f2
jing341 <takio0116@gmail.com>
2020-09-25 09:58:03 +0900

    최종 완성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css'
+
+function About(props) {
+    console.log(props);
+
+    return (
+        <div className="about__container">
+            <span>
+            "This movie app is made by the great Lizard' (I) myself."
+            </span>
+            <span>-Lizard-</span>
+        </div>
+    );
+}
+
+export default About;
