f20ee877f98542106b59ccb6602b8eb3f0a79599
jing341 <takio0116@gmail.com>
2020-09-30 21:27:44 +0900

    최종 완성 코드

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,7 +10,12 @@ class Detail extends React.Component {
     render() {
         const { location } = this.props;
         if (location.state) {
-        return <span>{location.state.title.year.summary}</span>;
+        return (
+            <div>
+        <span>{location.state.title}</span>
+        <span>{location.state.summary}</span>
+        </div>
+        )
     } else {
         return null;
     }
